4fb4b5bb3c91979a23106390ab491636ca0a61d6
maxlrose <Qqq_12345>
2022-05-21 18:47:35 +0300

    // 33333333333 reset

--- src/main/java/com/example/gittest1/GitTest1Application.java
+++ src/main/java/com/example/gittest1/GitTest1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-// 33333333333 forked 1 new commit AND HERE NEW COMMIT
+// 33333333333 reset
 
 
 @SpringBootApplication
